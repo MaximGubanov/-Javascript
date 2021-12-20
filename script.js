421885dbba9c13f427e64dd502cd616dbfa79c16
unknown <m_gubanov@bk.ru>
2021-12-20 17:22:46 +0300

    Урок_2._ ООП_в_JavaScript

--- script.js
+++ script.js
@@ -102,11 +102,11 @@ class Showcase {
       new Good({id: 1, title: 'Футболка', price: 140, img: '1'}),
       new Good({id: 2, title: 'Кроссовки', price: 320, img: '4'}),
       new Good({id: 3, title: 'Куртка', price: 24, img: '3'}),
-      new Good({id: 4, title: 'Футболка', price: 140, img: '1'}),
-      new Good({id: 5, title: 'Кроссовки', price: 320, img: '4'}),
-      new Good({id: 6, title: 'Куртка', price: 24, img: '3'}),
-      new Good({id: 7, title: 'Носки', price: 320, img: '2'}),
-      new Good({id: 8, title: 'Куртка', price: 24, img: '3'}),
+      new Good({id: 4, title: 'Футболка', price: 130, img: '6'}),
+      new Good({id: 5, title: 'Кроссовки', price: 390, img: '5'}),
+      new Good({id: 6, title: 'Куртка', price: 24, img: '7'}),
+      new Good({id: 7, title: 'Носки', price: 25, img: '2'}),
+      new Good({id: 8, title: 'Кроссовки', price: 245, img: '8'}),
     ]
   }
 
@@ -124,8 +124,8 @@ class RenderShowCase {
     this.goodsList = goodsList;
   }
 
-  renderItem({title, price, img}) {
-    return `<div class="goods-item"><img src="${img}.jpg" alt=""><h3>${title}</h3><p>Цена: ${price}</p><p><a href="#" class="add-button">Добавить</a></p></p></div>`;
+  renderItem({id, title, price, img}) {
+    return `<div class="goods-item"><img src="img/${img}.jpg" alt=""><h3>${title}</h3><p>Цена: ${price}</p><p><a href="#" id="${id}" class="add-button">Добавить</a></p></p></div>`;
   }
 
   render() {
@@ -144,7 +144,7 @@ class RenderCart {
   }
 
   renderItem({id, good, count}) {
-    return `<tr><td>${id}</td><td>${good.title}</td><td>${good.price}</td><td>${count}</td><td><a href="">Удалить</a></td></tr>`;
+    return `<tr><td>${id}</td><td>${good.title}</td><td>${good.price}</td><td>${count}</td><td><a href="" id="${id} class="remove-button">Удалить</a></td></tr>`;
   }
 
   render() {
@@ -170,10 +170,13 @@ showcase.addToCart(1)
 showcase.addToCart(3)
 showcase.addToCart(2)
 showcase.addToCart(2)
-// cart.remove(1)
+showcase.addToCart(4)
+showcase.addToCart(4)
+cart.remove(1)
 
 const rendershowcase = new RenderShowCase(goods)
 rendershowcase.render()
 
 const rendercart = new RenderCart(cartGoods.list)
 rendercart.render()
+
